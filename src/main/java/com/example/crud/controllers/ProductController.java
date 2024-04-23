056bbc17dc6cd0a49849c17d346ace945b8289bf
franciscocolatino <franciscocolatinodelima@gmail.com>
2024-04-22 21:50:36 -0300

    finished crud

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -3,12 +3,16 @@ package com.example.crud.controllers;
 import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProduct;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -27,5 +31,33 @@ public class ProductController {
         Product newProduct = new Product(data);
         repository.save(newProduct);
         return ResponseEntity.ok(newProduct);
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<Product> updateProduct(@RequestBody @Valid RequestProduct data) {
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Product> deleteProduct(@PathVariable String id) {
+        Optional<Product> optionalProduct = repository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            repository.delete(product);
+            return ResponseEntity.ok(product);
+        } else {
+            throw new EntityNotFoundException();
+        }
+
     }
 }
